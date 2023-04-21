b3eb4c389f83bf82a34f76a59c68459a9f59d7c8
Vakhrushev <vakhrushevke@lorena-kuhni.ru>
2023-04-21 08:07:57 +0500

    fix hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+	if IsDigit(rune(str[0])) {
+		return "", ErrInvalidString
+	}
+
+	sb := strings.Builder{}
+	r := []rune(str)
+
+	for i := 1; i <= len(r)-1; i++ {
+		cur := r[i]
+		prev := r[i-1]
+		switch {
+		case IsDigit(cur) && !IsDigit(prev):
+			num, _ := strconv.Atoi(string(cur))
+			text := strings.Repeat(string(prev), num)
+			sb.WriteString(text)
+		case IsDigit(cur) && IsDigit(prev):
+			return "", ErrInvalidString
+		case IsDigit(prev) && !IsDigit(cur):
+			continue
+		case !IsDigit(cur) && !IsDigit(prev):
+			sb.WriteRune(prev)
+		}
+	}
+
+	if !IsDigit(r[len(r)-1]) {
+		sb.WriteRune(r[len(r)-1])
+	}
+	return sb.String(), nil
+}
+
+func IsDigit(r rune) bool {
+	if r > 47 && r < 58 {
+		return true
+	}
+	return false
 }
